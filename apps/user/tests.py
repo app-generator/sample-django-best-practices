75c45ee4f5bc158c56958de9a96f80b60b98c385
koladev <onaelmangabo@gmail.com>
2022-05-11 12:14:59 +0100

    ✅ add create user test

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from apps.user.models import User
+
+
+@pytest.mark.django_db
+def test_create_user():
+    data_user = {
+        "username": "test_user",
+        "email": "test@gmail.com",
+        "password": "test_password",
+    }
+    
+    user = User.objects.create_user(**data_user)
+    
+    assert data_user["username"] == user.username
+    assert data_user["email"] == user.email
+    
