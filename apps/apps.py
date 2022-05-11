67621adeb2b29daccc677b55312f33d8bdc594d9
koladev <onaelmangabo@gmail.com>
2022-05-11 07:52:50 +0100

    ✨ add authentication

--- apps/apps.py
+++ apps/apps.py
@@ -1,6 +1,8 @@
+from cProfile import label
 from django.apps import AppConfig
 
 
 class AppsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps'
+    label = 'apps'
